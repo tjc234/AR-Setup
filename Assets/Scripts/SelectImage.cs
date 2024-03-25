0a7d5d96a9a1f313c838fd3ea7d82bad7052d559
tjc234 <tjc234@nau.edu>
2024-03-25 16:14:25 -0700

    AR Gallery 2 Complete

---
+++ Assets/Scripts/SelectImage.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SelectImageMode : MonoBehaviour
+{
+    void OnEnable()
+        {
+            UIController.ShowUI("SelectImage");
+        }
+}
