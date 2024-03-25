117d39c52848a570a666229cc2dd303c17a08e51
tjc234 <tjc234@nau.edu>
2024-03-25 12:06:17 -0700

    placing objects on plane

--- Assets/Scripts/PlaceObjectOnPlane.cs
+++ Assets/Scripts/PlaceObjectOnPlane.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class PlaceObjectOnPlane : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject placedPrefab;
+    private GameObject spawnedObject;
+    private ARRaycastManager raycaster;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
     void Start()
     {
-        
+        raycaster = GetComponent<ARRaycastManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnPlaceObject(InputValue value)
     {
-        
+        Vector2 touchPosition = value.Get<Vector2>();
+
+        if (raycaster.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
+            Pose hitPose = hits[0].pose;
+
+            if (spawnedObject == null)
+            {
+                spawnedObject = Instantiate(placedPrefab, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+                spawnedObject.transform.SetPositionAndRotation(hitPose.position, hitPose.rotation);
+            }
+        }
     }
 }
